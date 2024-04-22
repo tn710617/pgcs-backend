fdebc3558c37be90cf7c03fc9eddb07626205cc0
Ray <tn710617@gmail.com>
2024-04-22 23:29:46 +0800

    [log] added log

---
+++ app/Http/Middleware/LogReqRes.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogReqRes
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        $cloneResponse = clone($response);
+        $cloneRequest = clone($request);
+
+        $this->maskSensitiveInfo($cloneRequest, $cloneResponse);
+
+        if ($cloneRequest->hasHeader('Authorization')) {
+            $cloneRequest->headers->set('Authorization', 'Bearer ******');
+        }
+
+        Log::debug(
+            PHP_EOL.'Request:'.PHP_EOL.
+            PHP_EOL.
+            $cloneRequest->getMethod().' '.$cloneRequest->getRequestUri().' '.$cloneRequest->getProtocolVersion().
+            PHP_EOL.
+            $cloneRequest->headers.
+            'Cookies:'.PHP_EOL.
+            json_encode($cloneRequest->cookie()).
+            PHP_EOL.
+            PHP_EOL.
+            json_encode($cloneRequest->all()).
+            PHP_EOL.
+            PHP_EOL.'====================分隔線===================='.PHP_EOL.
+            PHP_EOL.
+            'Response:'.PHP_EOL.
+            PHP_EOL.
+            $cloneResponse.
+            PHP_EOL.
+            PHP_EOL.'====================結束線===================='.PHP_EOL.
+            PHP_EOL
+        );
+
+        return $response;
+    }
+
+    private function maskSensitiveInfo(&$cloneRequest, $cloneResponse)
+    {
+        if (app()->isProduction()) {
+            if ($cloneRequest->has('password')) {
+                $cloneRequest = $cloneRequest->merge(['password' => '******']);
+            }
+            if ($cloneRequest->cookie('one_dream_user_refresh_token')) {
+                $cloneRequest->cookies->set('one_dream_user_refresh_token', '******');
+            }
+
+            if (Arr::has($cloneResponse->getOriginalContent(), 'data.access_token')) {
+                $content = $cloneResponse->getOriginalContent();
+                $content['data']['access_token'] = '******';
+                $cloneResponse->setContent(json_encode($content));
+            }
+
+            $cloneResponse->cookie('one_dream_user_refresh_token', 'masked');
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,16 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Auth::viaRequest('simple', function (Request $request) {
-            Log::info('log detail', [
-                'bearer' => $request->bearerToken(),
-                'route' => $request->route()->uri,
-            ]);
-
             if (!$request->bearerToken()) {
                 return null;
             }
 
-            $user = User::find($request->bearerToken());
             return User::find($request->bearerToken());
         });
     }
